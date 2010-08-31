8e114c569e7c1b3090b6f07389255ba7db473a62
Jeremy Ashkenas <jashkenas@gmail.com>
2010-08-31 11:30:25 -0400

    Nuked the stateMachine. Made a DV.DocumentViewer class.

--- public/javascripts/DV/controllers/documentViewer.js
+++ public/javascripts/DV/controllers/documentViewer.js
@@ -1,11 +1,68 @@
-DV.controller   = new DV.StateMachine(DV.Schema.states,
-  {
-    events      : DV.Schema.events,
-    elements    : DV.Schema.elements,
-    helpers     : DV.Schema.helpers,
-    models      : DV.Schema.models
-  }
-);
+DV.DocumentViewer = function() {
+  this.events     = DV.Schema.events;
+  this.helpers    = DV.Schema.helpers;
+  this.models     = DV.Schema.models;
+  this.states     = DV.Schema.states;
+
+  // state values
+  this.pendingElements    = DV.Schema.elements;
+  this.isFocus            = true;
+  this.activeElement      = null;
+  this.observers          = [];
+  this.windowDimensions   = {};
+  this.scrollPosition     = null;
+  this.checkTimer         = {};
+  this.busy               = false;
+  this.annotationToLoadId = null;
+  this.dragReporter       = null;
+  this.compiled           = {};
+  this.tracker            = {};
+
+  this.events     = _.extend(this.events, {
+    application : this,
+    states      : this.states,
+    elements    : this.elements,
+    helpers     : this.helpers,
+    models      : this.models,
+    // this allows us to bind events to call the method corresponding to the current state
+    compile     : function(){
+      var a           = this.application;
+      var methodName  = arguments[0];
+      return function(){
+        if(!a.events[a.state][methodName]){
+          a.events[methodName].apply(a.events,arguments);
+        }else{
+          a.events[a.state][methodName].apply(a.events,arguments);
+        }
+      };
+    }
+  });
+
+  this.helpers  = _.extend(this.helpers, {
+    application : this,
+    states      : this.states,
+    elements    : this.elements,
+    events      : this.events,
+    models      : this.models
+  });
+
+  this.states   = _.extend(this.states, {
+    application : this,
+    helpers     : this.helpers,
+    elements    : this.elements,
+    events      : this.events,
+    models      : this.models
+  });
+};
+
+// Transition to a given state ... unless we're already in it.
+DV.Viewer.prototype.open = function(state) {
+  if (this.state == state) return;
+  this.state = state;
+  this.states[state].apply(this, arguments);
+};
+
+DV.controller = new DV.DocumentViewer();
 
 // The origin function, kicking off the entire documentViewer render.
 DV.load = function(documentRep, options) {
