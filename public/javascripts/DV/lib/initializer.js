38d444947094e42a55bb5d43f7457682632ba288
Jeremy Ashkenas <jashkenas@gmail.com>
2010-09-01 15:28:18 -0400

    fix the console.log shim for jQuery

--- public/javascripts/DV/lib/initializer.js
+++ public/javascripts/DV/lib/initializer.js
@@ -1,6 +1,6 @@
 // Fake out console.log for safety, if it doesn't exist.
-window.console = window.console || {};
-console.log    = console.log || _.identity;
+window.console || (window.console = {});
+console.log    || (console.log = _.identity);
 
 // Create the DV namespaces.
 window.DV   = window.DV   || {};
