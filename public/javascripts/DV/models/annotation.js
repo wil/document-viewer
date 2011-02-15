d2429327083f6ae99569ba4d110f887d427d4a57
Samuel Clay <samuel@ofbrooklyn.com>
2011-02-15 18:13:52 -0500

    Odd merge conflict around replacing/refreshing annotations.

--- public/javascripts/DV/models/annotation.js
+++ public/javascripts/DV/models/annotation.js
@@ -105,12 +105,8 @@ DV.model.Annotations.prototype = {
   // The document and list views.
   refreshAnnotation : function(anno) {
     var viewer = this.viewer;
-    DV.jQuery('#DV-annotation-' + anno.id + ', #DV-listAnnotation-' + anno.id).each(function() {
-      viewer.$('.DV-annotationTitleInput', this).val(anno.title);
-      viewer.$('.DV-annotationTitle', this).text(anno.title);
-      viewer.$('.DV-annotationTextArea', this).val(anno.text);
-      viewer.$('.DV-annotationBody', this).html(anno.text);
-    });
+    anno.html = this.render(anno);
+    DV.jQuery.$('#DV-annotation-' + anno.id).replaceWith(anno.html);
   },
 
   // Removes a given annotation from the Annotations model (and DOM).
