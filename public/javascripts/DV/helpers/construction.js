d26a1420443de9531d4e830ff9828615af828e5b
Jeremy Ashkenas <jashkenas@gmail.com>
2011-02-15 11:47:45 -0500

    further restyling ... adding a viewer-mini-debug

--- public/javascripts/DV/helpers/construction.js
+++ public/javascripts/DV/helpers/construction.js
@@ -190,7 +190,7 @@ _.extend(DV.Schema.helpers, {
 
     // Check if the zoom is showing, and if not, shorten the width of search
     _.defer(_.bind(function() {
-      if (this.elements.viewer.width() <= 650) {
+      if (this.elements.viewer.width() <= 700) {
         this.viewer.$('.DV-controls').addClass('DV-narrowControls');
       }
     }, this));
