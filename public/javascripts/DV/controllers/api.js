09a1e9a3cbb87ffc63535ff9b81022938fb66911
Al Shaw <almshaw@gmail.com>
2011-09-28 13:22:59 -0400

    api getters for annotations

--- public/javascripts/DV/controllers/api.js
+++ public/javascripts/DV/controllers/api.js
@@ -184,6 +184,18 @@ DV.Api.prototype = {
       this.viewer.pageSet.reflowPages();
     }
   },
+  
+  getAnnotationsBySortOrder : function() {
+    return this.viewer.models.bySortOrder;
+  },
+  
+  getAnnotationsByPage : function(page) {
+    return this.viewer.models.annotations.byPage[page];
+  },
+  
+  getAnnotationById : function(id) {
+    return this.viewer.models.annotations.ById[id];
+  },
 
   // Add a new annotation to the document, prefilled to any extent.
   addAnnotation : function(anno) {
