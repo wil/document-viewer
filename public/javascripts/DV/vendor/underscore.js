b23babac9e1680fc5e60c24e61fa9b3adb7b59fb
Ted Han <ted@knowtheory.net>
2012-01-11 10:36:37 -0600

    Remove AMD from the viewer's version of underscore, 'cause it will never need it.

--- public/javascripts/DV/vendor/underscore.js
+++ public/javascripts/DV/vendor/underscore.js
@@ -48,18 +48,9 @@
   // Create a safe reference to the Underscore object for use below.
   var _ = function(obj) { return new wrapper(obj); };
 
-  // Export the Underscore object for **Node.js**, with
-  // backwards-compatibility for the old `require()` API. If we're in
-  // the browser, add `_` as a global object via a string identifier,
+  // If we're in the browser, add `_` as a global object via a string identifier,
   // for Closure Compiler "advanced" mode.
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = _;
-    }
-    exports._ = _;
-  } else {
-    root['_'] = _;
-  }
+  root['_'] = _;
 
   // Current version.
   _.VERSION = '1.2.4';
